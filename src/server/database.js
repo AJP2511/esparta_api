70a63996ac4ae894737ac73edf0722cc26d29bdb
AJP2511 <johnson2511@gmail.com>
2021-03-04 00:23:57 -0300

    db

--- src/server/database.js
+++ src/server/database.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
-mongoose
-  .connect("mongodb+srv://espartauser:toddy1506@cluster0.pflcx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+mongoose.connect(
+  "mongodb+srv://esparta:toddy1506@cluster0.pflcx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
-  })
-  
+  }
+);
 
 module.exports = mongoose;
