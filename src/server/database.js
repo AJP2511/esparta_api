fa83436d97e3d6c17351904bbfbcf9f4926c39c6
AJP2511 <johnson2511@gmail.com>
2021-03-04 00:27:07 -0300

    db

--- src/server/database.js
+++ src/server/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb+srv://esparta:toddy1506@cluster0.pflcx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  "mongodb+srv://esparta:toddy1506@cluster0.pflcx.mongodb.net/todo?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
